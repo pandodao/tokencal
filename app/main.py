5bc7898e4f8bbb116860180b8505e766cbc24d24
Wen Sun <iwendellsun@gmail.com>
2023-02-22 17:04:35 +0900

    Add docker files

---
+++ app/main.py
@@ -0,0 +1,27 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import GPT2TokenizerFast
+
+app = FastAPI()
+tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+
+class CalData(BaseModel):
+    s: str
+
+@app.get("/")
+def root():
+    return "OK"
+
+@app.get("/token")
+def token_cal_get(s: str):
+    if s == "":
+        raise HTTPException(status_code=401, detail="request parameter is empty")
+    return {"s": s, "token": len(tokenizer(s)["input_ids"])}
+
+@app.post("/token")
+def token_cal_post(cd: CalData):
+    if cd.s == "":
+        raise HTTPException(status_code=401, detail="request parameter is empty")
+    return {"s": cd.s, "token": len(tokenizer(cd.s)["input_ids"])}
